ccb2dcaf5daea3cdf79a08977a091049d0bb4d06
phonglex <fonglex@gmail.com>
2021-09-20 21:15:03 +0700

    update domain config

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['18.117.169.136']
 
 
 # Application definition
