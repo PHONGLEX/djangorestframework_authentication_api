e319af1ecd3b50ddff36b13feb789b221834dfb7
phonglex <fonglex@gmail.com>
2021-09-21 07:32:13 +0700

    update config

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.117.169.136']
 
