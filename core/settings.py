35836fe3f296523d6e49de700fc91576db0f2b71
phonglex <fonglex@gmail.com>
2021-09-21 07:55:56 +0700

    update config

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '10.0.1.33', '18.117.169.136']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
